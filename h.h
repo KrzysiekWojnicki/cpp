a474936a477d538d175dc60d436f851622277aed
Krzysiek <31898935+KrzysiekWojnicki@users.noreply.github.com>
2017-09-15 17:28:55 +0200

    Add files via upload

---
+++ h.h
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Wyswietl // {klasa} {nazwa} 
+{
+	public: 
+		int ile;
+		float a ,b;
+
+		void wstemp();
+		void pentla();
+		void size();
+		void dodawanie(float,float);
+		void menu();
+}; // klase konczymy zawsze " ; " znakiem.
+
